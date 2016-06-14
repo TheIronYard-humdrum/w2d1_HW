94be6ba16982a31e414e27b00e1f8ce0f5b5e6a5
humdrum <jjrajani@gmail.com>
2016-06-14 09:20:33 -0400

    update hardmode to accept infinite numbers as argumet)

--- Homework/hard.js
+++ Homework/hard.js
@@ -2,9 +2,9 @@ var sumOfSquares = function(x, y, z) {
   numbers = [x, y, z];
   numbers = numbers.sort();
 
-  sum = (Math.pow(numbers[2], 2) + Math.pow(numbers[1], 2));
+  sum = (Math.pow(numbers[numbers.length-1], 2) + Math.pow(numbers[numbers.length-2], 2));
   return sum;
-}
+};
 
 // I'm the worst at remembering my punctuation since coding in
 // Ruby for the last couple months.  But I'll get back into it!
@@ -16,13 +16,28 @@ sumOfSquares(12, 1, 15)
 var sumOfSquares = function(x, y, z) {
   if (x <= y && x <= z) {
     return (y*y + z*z);
-  }
+  };
   else if (y <= x && y <= z) {
     return (x*x + z*z);
-  }
+  };
   else {
     return (x*x + y*y);
-  }
-}
+  };
+};
 
 sumOfSquares(12, 1, 15)
+
+// Solution for infinite number of given numbers
+
+var sumOfSquares = function(numbers) {
+  nums = numbers.sort(function(a, b){return a-b});
+  sum = (Math.pow(numbers[numbers.length-1], 2) + Math.pow(numbers[numbers.length-2], 2));
+  return sum
+};
+
+// I'm the worst at remembering my punctuation since coding in
+// Ruby for the last couple months.  But I'll get back into it!
+numbers = [1,4,7,3,9,3,8,12,45,78,23]
+sumOfSquares(numbers)
+
+
